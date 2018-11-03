ec78f022372f59f4ec4bf343de27cbcad98d360a
ryan57hb <ryan57@uic.edu>
2018-11-03 18:32:55 -0500

    fix: finally fix all the indent issues

--- project-tv-script-generation/problem_unittests.py
+++ project-tv-script-generation/problem_unittests.py
@@ -27,18 +27,17 @@ class AssertTest(object):
     def test(self, assert_condition, assert_message):
         assert assert_condition, assert_message + '\n\nUnit Test Function Parameters\n' + self.assert_param_message
 
-        
-        
+
 def test_create_lookup_tables(create_lookup_tables):
     test_text = '''
-    Moe_Szyslak Moe's Tavern Where the elite meet to drink
-    Bart_Simpson Eh yeah hello is Mike there Last name Rotch
-    Moe_Szyslak Hold on I'll check Mike Rotch Mike Rotch Hey has anybody seen Mike Rotch lately
-    Moe_Szyslak Listen you little puke One of these days I'm gonna catch you and I'm gonna carve my name on your back  with an ice pick
-    Moe_Szyslak Whats the matter Homer You're not your normal effervescent self
-    Homer_Simpson I got my problems Moe Give me another one
-    Moe_Szyslak Homer hey you should not drink to forget your problems
-    Barney_Gumble Yeah you should only drink to enhance your social skills'''
+        Moe_Szyslak Moe's Tavern Where the elite meet to drink
+        Bart_Simpson Eh yeah hello is Mike there Last name Rotch
+        Moe_Szyslak Hold on I'll check Mike Rotch Mike Rotch Hey has anybody seen Mike Rotch lately
+        Moe_Szyslak Listen you little puke One of these days I'm gonna catch you and I'm gonna carve my name on your back with an ice pick
+        Moe_Szyslak Whats the matter Homer You're not your normal effervescent self
+        Homer_Simpson I got my problems Moe Give me another one
+        Moe_Szyslak Homer hey you should not drink to forget your problems
+        Barney_Gumble Yeah you should only drink to enhance your social skills'''
     
     test_text = test_text.lower()
     test_text = test_text.split()
@@ -55,18 +54,18 @@ def test_create_lookup_tables(create_lookup_tables):
     assert len(vocab_to_int) == len(int_to_vocab),\
         'Length of vocab_to_int and int_to_vocab don\'t match. ' \
         'vocab_to_int is length {}. int_to_vocab is length {}'.format(len(vocab_to_int), len(int_to_vocab))
-    
+
     # Make sure the dicts have the same words
     vocab_to_int_word_set = set(vocab_to_int.keys())
     int_to_vocab_word_set = set(int_to_vocab.values())
-    
+
     assert not (vocab_to_int_word_set - int_to_vocab_word_set),\
         'vocab_to_int and int_to_vocab don\'t have the same words.' \
         '{} found in vocab_to_int, but not in int_to_vocab'.format(vocab_to_int_word_set - int_to_vocab_word_set)
     assert not (int_to_vocab_word_set - vocab_to_int_word_set),\
         'vocab_to_int and int_to_vocab don\'t have the same words.' \
         '{} found in int_to_vocab, but not in vocab_to_int'.format(int_to_vocab_word_set - vocab_to_int_word_set)
-    
+
     # Make sure the dicts have the same word ids
     vocab_to_int_word_id_set = set(vocab_to_int.values())
     int_to_vocab_word_id_set = set(int_to_vocab.keys())
@@ -77,19 +76,21 @@ def test_create_lookup_tables(create_lookup_tables):
     assert not (int_to_vocab_word_id_set - vocab_to_int_word_id_set),\
         'vocab_to_int and int_to_vocab don\'t contain the same word ids.' \
         '{} found in int_to_vocab, but not in vocab_to_int'.format(int_to_vocab_word_id_set - vocab_to_int_word_id_set)
-    
-    #Make sure the dicts make the same lookup
+
+    # Make sure the dicts make the same lookup
     missmatches = [(word, id, id, int_to_vocab[id]) for word, id in vocab_to_int.items() if int_to_vocab[id] != word]
     
     assert not missmatches,\
-        'Found {} missmatche(s). First missmatch: vocab_to_int[{}] = {} and int_to_vocab[{}] = {}'.format(len(missmatches),
+        'Found {} missmatche(s). First missmatch: vocab_to_int[{}] = {} and int_to_vocab[{}] = {}'.format(
+                                                                                                          len(missmatches),
                                                                                                           *missmatches[0])
+    
     assert len(vocab_to_int) > len(set(test_text))/2,\
         'The length of vocab seems too small.  Found a length of {}'.format(len(vocab_to_int))
     
     _print_success_message()
-    
-    
+
+
 def test_tokenize(token_lookup):
     symbols = set(['.', ',', '"', ';', '!', '?', '(', ')', '-', '\n'])
     token_dict = token_lookup()
@@ -103,24 +104,24 @@ def test_tokenize(token_lookup):
     unknown_symbols = set(token_dict.keys()) - symbols
 
     assert not missing_symbols, \
-    'Missing symbols: {}'.format(missing_symbols)
+        'Missing symbols: {}'.format(missing_symbols)
     assert not unknown_symbols, \
         'Unknown symbols: {}'.format(unknown_symbols)
 
-# Check values type
+    # Check values type
     bad_value_type = [type(val) for val in token_dict.values() if not isinstance(val, str)]
     
     assert not bad_value_type,\
         'Found token as {} type.'.format(bad_value_type[0])
 
-# Check for spaces
+    # Check for spaces
     key_has_spaces = [k for k in token_dict.keys() if ' ' in k]
     val_has_spaces = [val for val in token_dict.values() if ' ' in val]
     
     assert not key_has_spaces,\
         'The key "{}" includes spaces. Remove spaces from keys and values'.format(key_has_spaces[0])
     assert not val_has_spaces,\
-    'The value "{}" includes spaces. Remove spaces from keys and values'.format(val_has_spaces[0])
+        'The value "{}" includes spaces. Remove spaces from keys and values'.format(val_has_spaces[0])
     
     # Check for symbols in values
     symbol_val = ()
@@ -130,7 +131,7 @@ def test_tokenize(token_lookup):
                 symbol_val = (symbol, val)
 
     assert not symbol_val,\
-    'Don\'t use a symbol that will be replaced in your tokens. Found the symbol {} in value {}'.format(*symbol_val)
+        'Don\'t use a symbol that will be replaced in your tokens. Found the symbol {} in value {}'.format(*symbol_val)
     
     _print_success_message()
 
@@ -170,7 +171,7 @@ def test_rnn(RNN, train_on_gpu):
                              'Sequence Length': sequence_length,
                              'Input': b})
 
-# initialization
+    # initialization
     correct_hidden_size = (n_layers, batch_size, hidden_dim)
     assert_condition = hidden[0].size() == correct_hidden_size
     assert_message = 'Wrong hidden state size. Expected type {}. Got type {}'.format(correct_hidden_size, hidden[0].size())
@@ -188,8 +189,8 @@ def test_rnn(RNN, train_on_gpu):
     assert_test.test(assert_condition, assert_message)
     
     _print_success_message()
-    
-    
+
+
 def test_forward_back_prop(RNN, forward_back_prop, train_on_gpu):
     batch_size = 200
     input_size = 20
@@ -227,3 +228,5 @@ def test_forward_back_prop(RNN, forward_back_prop, train_on_gpu):
     assert type(loss) == float, 'Wrong return type. Exptected {}, got {}'.format(float, type(loss))
     
     _print_success_message()
+
+
